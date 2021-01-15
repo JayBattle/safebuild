210177cf9ac16d91bf6b8fbad4c3e5f65a3d4011
root <jaywbattle@gmail.com>
2021-01-15 15:39:14 -0500

    First Build

---
+++ safebuild.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#This safely builds your docker image in a subdirectory inserting all variables found in .env files
+Image=$1
+
+echo Starting Safe Build...
+mkdir $Image
+cp * $Image
+cp .* $Image
+cd $Image
+source *.env
+export $(cut -d= -f1 *.env)
+for file in * ; do envsubst < $file | sponge $file ; done
+for file in .* ; do envsubst < $file | sponge $file ; done
+docker build -t $Image .
+cd ..
+rm -r $Image
+echo Done With Safe Build!
